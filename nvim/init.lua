dd7623c866830d5c83c83da5be4b7620533438d9
Sivanesh Ashok <sivaneshashok@gmail.com>
2025-01-18 16:12:17 +0530

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,21 @@
+require("options")
+
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
